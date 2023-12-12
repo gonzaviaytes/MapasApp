4b78749bac1fc5511c64d1e8cea21e1e199b61aa
Gonzalo Viaytes <gonzaviaytes@gmail.com>
2023-12-12 15:30:00 -0300

    App Mapas

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiKey: "pk.eyJ1IjoiZ29uemF2aWF5dGVzIiwiYSI6ImNsbmRnbHRzODAwZjYybHBnZnE4anZwbjAifQ.DjCJauI3InFmycQPxIisFg",
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+//import { environment } from './environments/environment';
+
+
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiZ29uemF2aWF5dGVzIiwiYSI6ImNsbmRnbHRzODAwZjYybHBnZnE4anZwbjAifQ.DjCJauI3InFmycQPxIisFg';
+
+
+
+if ( !navigator.geolocation ) {
+  alert("Navegador no soporta la Geolocation")
+  throw new Error("Navegador no soporta la Geolocation");
+}
+
+
+//if (environment.production) {
+  enableProdMode();
+//}
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
